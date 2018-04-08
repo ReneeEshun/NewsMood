df170d1d8ab0361858b2ad6731b9934dd9db5618
ReneeEshun <Cherie4840@yahoo.com>
2018-04-08 16:41:38 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+# Twitter API Keys
+consumer_key = "jJXNqTuYFGO8h6EKe6MwE0RRW"
+consumer_secret = "90WeyQLr9HD9PYCQEXcWH8aAS4SGvLR5vxt8eVuBQeFCzikJhi"
+access_token = "980969804998656000-PgQtr7xTWnLhSR7yW43JxoEZzQdB5ZN"
+access_token_secret = "pdmm6xutwxxB7I0hgRZ7yxEio2LRhIOouKlEIijVoeoNN"
+
+        
